e66c143517186e91ef732284a26cd05710f69ad0
Your Name <you@example.com>
2024-03-24 13:45:26 +0900

    userdata.sh修正

--- userdata.sh
+++ userdata.sh
@@ -6,8 +6,8 @@ sudo dnf install -y python3
 sudo dnf install -y python3-pip
 sudo pip3 install awscli
 
-# cloudwatch-agent、ssm-agentのインストール
-dnf install -y amazon-cloudwatch-agent amazon-ssm-agent
+# cloudwatch-agent、ssm-agent、jqのインストール
+dnf install -y amazon-cloudwatch-agent amazon-ssm-agent jq
 /opt/aws/amazon-cloudwatch-agent/bin/amazon-cloudwatch-agent-ctl -a fetch-config -m ec2 -c file:/opt/aws/amazon-cloudwatch-agent/etc/amazon-cloudwatch-agent.json -s
 systemctl enable amazon-ssm-agent
 systemctl start amazon-ssm-agent
@@ -22,7 +22,7 @@ sudo dnf install -y /tmp/ec2-instance-connect/ec2-instance-connect.rpm /tmp/ec2-
 sudo dnf install -y https://s3.amazonaws.com/streaming-data-agent/aws-kinesis-agent-latest.amzn2.noarch.rpm
 sudo usermod -G wheel aws-kinesis-agent-user
 sudo mv /etc/aws-kinesis/agent.json /etc/aws-kinesis/agent_bk.json
-aws ssm get-parameter --region ap-northeast-1 --name "/kinesis-agent/config" --output json | sudo tee /etc/aws-kinesis/agent.json
+aws ssm get-parameter --region ap-northeast-1 --name "/kinesis-agent/config"|jq -r .Parameter.Value|sudo tee /etc/aws-kinesis/agent.json
 sudo chmod 644 /etc/aws-kinesis/agent.json
 sudo service aws-kinesis-agent start
 sudo chkconfig aws-kinesis-agent on
