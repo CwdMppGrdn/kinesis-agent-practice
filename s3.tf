7080dff99c9349474ae9c703a7877adf168e9efa
Your Name <you@example.com>
2024-03-24 13:56:33 +0900

    S3.tf修正

--- s3.tf
+++ s3.tf
@@ -8,3 +8,18 @@ resource "aws_s3_bucket_public_access_block" "example" {
   ignore_public_acls      = true
   restrict_public_buckets = true
 }
+
+#検証終了時にS3バケット内のファイルを全削除する
+#参考：https://qiita.com/ChaseSan/items/11fe05926c700220d3cc
+resource "null_resource" "example" {
+  triggers = {
+    bucket   = aws_s3_bucket.example.bucket
+  }
+  depends_on = [
+    aws_s3_bucket.example
+  ]
+  provisioner "local-exec" {
+    when = destroy
+    command = "aws s3 rm s3://${self.triggers.bucket} --recursive"
+  }
+}
